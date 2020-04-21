c752d137b118a966ecf074fe9b941df90131d741
Dat Vuong <dat.vuong007@gmail.com>
2020-04-21 23:44:00 +0200

    Update Reddit Clean - April 20.R
    Final version of Reddit data

--- Reddit Clean - April 20.R
+++ Reddit Clean - April 20.R
@@ -9,6 +9,9 @@ library(lubridate)
 
 Reddit_Topic <- read_csv("Reddit - CoronavirusUS - April 20.csv")
 
+# Reddit_Topic <- read_csv("Reddit Submission - Coronavirus - April 20.csv")
+
+
 Reddit_Topic_column <- map_dbl(Reddit_Topic, function(x){
   sum(is.na(x))
 }) %>%
@@ -21,18 +24,21 @@ Reddit_Topic_column <- map_dbl(Reddit_Topic, function(x){
   as.vector()
 
 # Save comments one more time
-Reddit_Topic <- Reddit_Topic %>%
+Reddit_Topic2 <- Reddit_Topic %>%
   mutate(created_utc = as_datetime(created_utc),
          retrieved_on = as_datetime(retrieved_on))  %>%
   select(Reddit_Topic_column, -all_awardings, -author_flair_richtext, -awarders, -gildings, -total_awards_received, -link_flair_richtext, 
-         -author_flair_type, -author_fullname, -link_flair_text_color, -link_flair_type, -full_link, -permalink) %>%
+         -author_flair_type, -author_fullname, -link_flair_text_color, -link_flair_type, -full_link, -permalink, link_flair_text) %>%
   distinct(id, created_utc, retrieved_on, title, .keep_all = T) %>%
   select_if(function(a) {is.numeric(a)|is.character(a)})
 
+which(Reddit_Topic == "Europe", arr.ind = T)
 
 # Reddit Comments ---------------------------------------------------------
 
 Reddit_Comment <- read_csv("Reddit Comment - CoronavirusUS - April 20.csv")
+
+# Reddit_Comment <- read_csv("Reddit Comment - Coronavirus - April 20.csv")
 
 Reddit_Comment_column <- map_dbl(Reddit_Comment, function(x){
   sum(is.na(x))
@@ -46,13 +52,16 @@ Reddit_Comment_column <- map_dbl(Reddit_Comment, function(x){
   as.vector()
 
 # Save comments one more time
-Reddit_Comment2 <- Reddit_Comment %>%
+Reddit_Comment <- Reddit_Comment %>%
   mutate(created_utc = as_datetime(created_utc),
          retrieved_on = as_datetime(retrieved_on)) %>%
   select(Reddit_Comment_column, -all_awardings, -author_flair_richtext, -awarders, -gildings, -total_awards_received) %>%
   mutate(id_2 = str_sub(link_id, start = 4, end = 9)) %>%
   select(id_2, comment_author = author, comments = body, comment_day = created_utc, is_submitter,  retrieved_comments_on = retrieved_on, send_replies) %>%
-  filter(!str_detect(comments, "^\\[[:lower:]+\\]$"), str_detect(comments, "[:alnum:]"))
+  filter(!str_detect(comments, "^\\[[:lower:]+\\]$")) %>%
+  mutate(comments = rm_url(comments, clean = T)) %>%
+  mutate(comments = str_replace_all(comments, "[^[:alnum:]|^[:blank:]]", "")) %>%
+  filter(str_count(comments, pattern = boundary("word")) > 2)
 
 
 # Combine  ----------------------------------------------------------------
@@ -69,23 +78,6 @@ names(Reddit_Comment_Final)
 
 Reddit_Comment_Final$link_flair_text
 
-write_csv(Reddit_Comment_Final, "Reddit_Comment_April 11.csv")
-# Reddit_Topic %>%
-#   distinct(id, created_utc, retrieved_on, title, .keep_all = T) %>%
-#   nrow()
-# 
-# nrow(Reddit_Topic)
-# 
-# Reddit_Topic %>%
-#   count(id) %>%
-#   nrow()
-# 
-# names(Reddit_Topic)
-
-
-
-
-
-
+write_csv2(Reddit_Comment_Final, "Reddit CoronavirusUS - Final Data - April 20")
 
 
