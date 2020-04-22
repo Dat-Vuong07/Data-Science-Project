cc8517d2f8e12afae08ae2e042a37876c8847340
Dat Vuong <dat.vuong007@gmail.com>
2020-04-22 21:16:48 +0200

    Update Reddit Clean - April 20.R
    Update Version  to run the code

--- Reddit Clean - April 20.R
+++ Reddit Clean - April 20.R
@@ -6,6 +6,7 @@ library(readxl)
 library(readr)
 library(tidyverse)
 library(lubridate)
+library(qdapRegex)
 
 Reddit_Topic <- read_csv("Reddit - CoronavirusUS - April 20.csv")
 
@@ -24,15 +25,13 @@ Reddit_Topic_column <- map_dbl(Reddit_Topic, function(x){
   as.vector()
 
 # Save comments one more time
-Reddit_Topic2 <- Reddit_Topic %>%
+Reddit_Topic <- Reddit_Topic %>%
   mutate(created_utc = as_datetime(created_utc),
          retrieved_on = as_datetime(retrieved_on))  %>%
   select(Reddit_Topic_column, -all_awardings, -author_flair_richtext, -awarders, -gildings, -total_awards_received, -link_flair_richtext, 
          -author_flair_type, -author_fullname, -link_flair_text_color, -link_flair_type, -full_link, -permalink, link_flair_text) %>%
   distinct(id, created_utc, retrieved_on, title, .keep_all = T) %>%
   select_if(function(a) {is.numeric(a)|is.character(a)})
-
-which(Reddit_Topic == "Europe", arr.ind = T)
 
 # Reddit Comments ---------------------------------------------------------
 
@@ -78,6 +77,6 @@ names(Reddit_Comment_Final)
 
 Reddit_Comment_Final$link_flair_text
 
-write_csv2(Reddit_Comment_Final, "Reddit CoronavirusUS - Final Data - April 20")
+write_csv2(Reddit_Comment_Final, "Reddit CoronavirusUS - Final Data - April 20.csv")
 
-
+write_rds(Reddit_Comment_Final, "Reddit CoronavirusUS - Final Data - April 20.rds")
