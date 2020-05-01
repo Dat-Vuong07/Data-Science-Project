798d4d8d5bdcc27460b9b757566aa6c1efde4047
phongv <vietphong95@gmail.com>
2020-05-01 12:11:37 +0200

    Simple news clean

--- news_clean.R
+++ news_clean.R
@@ -4,8 +4,19 @@ news <- read.csv("news_corona.csv", stringsAsFactors = FALSE, na.strings = "")
 
 # Change variables types
 news$publish_date <- as.Date(news$publish_date)
+news <- news %>% 
+  filter(publish_date < "2020-04-20")
 news$source <- as.factor(news$source)
 glimpse(news[1,])
+
+# Set 'between' operator
+`%between%`<- function(x,rng) {x>=rng[1] & x<=rng[2]}
+
+# Check the length of text
+news_addlen <- news %>% 
+  mutate(len_text = str_length(text)) %>% 
+  filter(len_text %between% c(200, 10000))
+summary(news_addlen)
 
 # Function to expand contractions in an English-language source
 fix.contractions <- function(doc) {
@@ -27,15 +38,15 @@ fix.contractions <- function(doc) {
 text.clean <- function(txt) {
   txt <- trimws(txt)
   txt <- sapply(txt, fix.contractions)
-  txt <- str_remove_all(txt, "[^a-zA-Z0-9 ]")
-  txt <- tolower(txt)
   return(txt)
 }
 
 # Clean the variables
-news$title <- text.clean(news$title)
-news$summary <- text.clean(news$summary)
-news$keywords <- text.clean(news$keywords)
-news$text <- text.clean(news$text)
+news_addlen$title <- text.clean(news_addlen$title)
+news_addlen$summary <- text.clean(news_addlen$summary)
+news_addlen$keywords <- text.clean(news_addlen$keywords)
+news_addlen$text <- text.clean(news_addlen$text)
+news_save <- news_addlen %>% 
+  select(title, publish_date, source, text)
 
-saveRDS(news, "news_clean.Rds")
+saveRDS(news_save, "news_clean.Rds")
